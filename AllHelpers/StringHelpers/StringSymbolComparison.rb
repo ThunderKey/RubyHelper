cd36f675df9cc10259d9bf26db7b205adbe22114
Nicolas <nicolas@keltec.ch>
2012-03-26 10:29:02 +0300

    Update AllHelpers/StringHelpers/StringSymbolComparison.rb

--- AllHelpers/StringHelpers/StringSymbolComparison.rb
+++ AllHelpers/StringHelpers/StringSymbolComparison.rb
@@ -8,8 +8,9 @@ class Symbol
 	alias :eql? :==
 	def == other
 		if other.respond_to? :to_sym
-			return self.eql?(other.to_sym)
+			self.eql?(other.to_sym)
+		else
+			false
 		end
-		false
 	end
 end
